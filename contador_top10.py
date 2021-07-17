b8f8656acb5cb24a1c0dba51b582e4d7642f1764
alexandrelalaina <45208791+alexandrelalaina@users.noreply.github.com>
2021-07-17 15:34:57 -0300

    first commit

---
+++ contador_top10.py
@@ -0,0 +1,12 @@
+import sys
+from pyspark import SparkContext, SparkConf
+if __name__ == "__main__":
+    with open("resultado/part-00000.txt", encoding="utf8") as file:
+        data = file.read().split('\n')
+        data = data[:10]
+        top10 = ""
+        top10 = '\n'.join(data)
+        print(top10)
+
+    with open('resultado.txt', 'w') as newFile:
+        newFile.write(top10)
